a4c337efb24afe8e9d399edd716426d573443904
satoshihayashi2015 <satoshihayashi2015@gmail.com>
2015-05-09 21:55:50 +0900

    Macから移植完了

---
+++ Education/Game01/Assets/Scripts/Cell.cs
@@ -0,0 +1,234 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseDown(){
+		
+		//		// Change to Sprite 0
+		//		Sprite sp = GetSprite("Images/SpriteSample", "SpriteSample_0");
+		//
+		//		// SpriteRendererを取得する
+		//		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		//
+		//		// Spriteを変更する
+		//		sr.sprite = sp;
+		
+		print (name);
+		
+		string[] pos = name.Replace ("cell", "").Split (new string[]{"_"}, System.StringSplitOptions.RemoveEmptyEntries);
+		if (pos.Length != 2) {return;}
+		
+		string rowStr = pos[0];
+		string colStr = pos[1];
+		int row = 0;
+		int col = 0;
+		
+		if (!int.TryParse (rowStr, out row)) {return;}
+		if (!int.TryParse (colStr, out col)) {return;}
+		
+		TrySet (row,col);
+	}
+	
+	
+	// スプライトの取得
+	// @param fileName ファイル名
+	// @param spriteName スプライト名
+	public Sprite GetSprite(string fileName, string spriteName) {
+		Sprite[] sprites = Resources.LoadAll<Sprite>(fileName);
+		return System.Array.Find<Sprite>(sprites, (sprite) => sprite.name.Equals(spriteName));
+	}
+	
+	private void TrySet(int row, int col){
+		
+		if (Manager.fieldState [row, col] != Manager.CellStateType.FIELD) {return;}
+		if (!CanSandwich(row,col)) {return;}
+		
+		switch (Manager.gameMode) {
+		case Manager.GameMode.WHITE:
+			Manager.fieldState [row, col] = Manager.CellStateType.WHITE;
+			Manager.gameMode = Manager.GameMode.BLACK;
+			break;
+		case Manager.GameMode.BLACK:
+			Manager.fieldState [row, col] = Manager.CellStateType.BLACK;
+			Manager.gameMode = Manager.GameMode.WHITE;
+			break;
+		}
+		
+		Manager.DrawFieldS ();
+	}
+	
+	private bool CanSandwich(int row, int col){
+		
+		Manager.CellStateType player_type = (Manager.gameMode == Manager.GameMode.BLACK) ? Manager.CellStateType.BLACK : Manager.CellStateType.WHITE;
+		Manager.CellStateType enemy_type = (Manager.gameMode == Manager.GameMode.BLACK) ? Manager.CellStateType.WHITE : Manager.CellStateType.BLACK; 
+		
+		int current_row = row;
+		int current_col = col;
+		int get_enemy_count = 0;
+		
+		//north
+		while(current_row>0){
+			current_row--;
+			if (Manager.fieldState[current_row,current_col]==enemy_type) {
+				get_enemy_count++;
+			}
+			else if(Manager.fieldState[current_row,current_col]==player_type && get_enemy_count>0) {
+				return true;
+			}
+			else {
+				break;
+			}
+		}
+		
+		//reset
+		current_row = row;
+		current_col = col;
+		get_enemy_count = 0;
+		
+		//east
+		while(current_col<Manager.COL_COUNT-1){
+			current_col++;
+			if (Manager.fieldState[current_row,current_col]==enemy_type) {
+				get_enemy_count++;
+			}
+			else if(Manager.fieldState[current_row,current_col]==player_type && get_enemy_count>0) {
+				return true;
+			}
+			else {
+				break;
+			}
+		}
+		
+		//reset
+		current_row = row;
+		current_col = col;
+		get_enemy_count = 0;
+		
+		//south
+		while(current_row<Manager.ROW_COUNT-1){
+			current_row++;
+			if (Manager.fieldState[current_row,current_col]==enemy_type) {
+				get_enemy_count++;
+			}
+			else if(Manager.fieldState[current_row,current_col]==player_type && get_enemy_count>0) {
+				return true;
+			}
+			else {
+				break;
+			}
+		}
+		
+		//reset
+		current_row = row;
+		current_col = col;
+		get_enemy_count = 0;
+		
+		//west
+		while(current_col>0){
+			current_col--;
+			if (Manager.fieldState[current_row,current_col]==enemy_type) {
+				get_enemy_count++;
+			}
+			else if(Manager.fieldState[current_row,current_col]==player_type && get_enemy_count>0) {
+				return true;
+			}
+			else {
+				break;
+			}
+		}
+		
+		//reset
+		current_row = row;
+		current_col = col;
+		get_enemy_count = 0;
+		
+		//north west
+		while (current_row>0) {
+			current_row--;
+			while (current_col>0) {
+				current_col--;
+				if (Manager.fieldState [current_row, current_col] == enemy_type) {
+					get_enemy_count++;
+				} else if (Manager.fieldState [current_row, current_col] == player_type && get_enemy_count > 0) {
+					return true;
+				} else {
+					break;
+				}
+			}
+		}
+		
+		//reset
+		current_row = row;
+		current_col = col;
+		get_enemy_count = 0;
+		
+		//north east
+		while (current_row>0) {
+			current_row--;
+			while (current_col<Manager.COL_COUNT-1) {
+				current_col++;
+				if (Manager.fieldState [current_row, current_col] == enemy_type) {
+					get_enemy_count++;
+				} else if (Manager.fieldState [current_row, current_col] == player_type && get_enemy_count > 0) {
+					return true;
+				} else {
+					break;
+				}
+			}
+		}
+		
+		//reset
+		current_row = row;
+		current_col = col;
+		get_enemy_count = 0;
+		
+		//south west
+		while (current_row<Manager.ROW_COUNT-1) {
+			current_row++;
+			while (current_col>0) {
+				current_col--;
+				if (Manager.fieldState [current_row, current_col] == enemy_type) {
+					get_enemy_count++;
+				} else if (Manager.fieldState [current_row, current_col] == player_type && get_enemy_count > 0) {
+					return true;
+				} else {
+					break;
+				}
+			}
+		}
+		
+		//reset
+		current_row = row;
+		current_col = col;
+		get_enemy_count = 0;
+		
+		//south east
+		while (current_row<Manager.ROW_COUNT-1) {
+			current_row++;
+			while (current_col<Manager.COL_COUNT-1) {
+				current_col++;
+				if (Manager.fieldState [current_row, current_col] == enemy_type) {
+					get_enemy_count++;
+				} else if (Manager.fieldState [current_row, current_col] == player_type && get_enemy_count > 0) {
+					return true;
+				} else {
+					break;
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+}
---
+++ Education/Game01/Assets/Scripts/Manager.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+	
+	public GameObject cellPrefab;
+	
+	private const int CELL_SIZE = 64;
+	private const int FIELD_ROW_COUNT = 8; 
+	private const int FIELD_COL_COUNT = 8;
+	private const int WALL_COUNT = 1;
+	public const int ROW_COUNT = FIELD_ROW_COUNT + (WALL_COUNT * 2);
+	public const int COL_COUNT = FIELD_COL_COUNT + (WALL_COUNT * 2);
+	
+	public static string CELL_NAME_FORMAT = "cell{0}_{1}";
+	
+	public static GameMode gameMode = GameMode.BLACK;
+	public static CellStateType[,] fieldState = new CellStateType[ROW_COUNT, COL_COUNT];
+	
+	public enum GameMode{
+		NONE
+		,WHITE
+		,BLACK
+		,RESULT
+	}
+	
+	public enum CellStateType {
+		WALL
+		,FIELD
+		,WHITE
+		,BLACK
+		,RI
+		,BA
+		,A
+		,SHI
+		,TRANSPARENT
+	}
+	
+	// Use this for initialization
+	void Start () {
+		CreateField ();
+		DrawField ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	private void CreateField() {
+		for (int row = 0; row < ROW_COUNT; row++) {
+			for (int col = 0; col < COL_COUNT; col++) {
+				GameObject cell = (GameObject)Instantiate(cellPrefab);
+				
+				//Position Setting
+				Vector3 p = cell.transform.position;
+				p.y = (float)(-row * CELL_SIZE * 0.01);
+				p.x = (float)(+col * CELL_SIZE * 0.01);
+				cell.transform.position = p;
+				
+				cell.name = string.Format(CELL_NAME_FORMAT, row, col);
+				
+				fieldState[row,col] = CellStateType.FIELD;
+				
+				//Wall
+				if (row==0 ||row==ROW_COUNT-1 || col==0 || col==COL_COUNT-1) {
+					fieldState[row,col] = CellStateType.WALL;
+				}
+				
+				//Black and White
+				if ((row==4 && col==5) || (row==5 && col==4)) {
+					fieldState[row,col] = CellStateType.BLACK;
+				}
+				if ((row==4 && col==4) || (row==5 && col==5)) {
+					fieldState[row,col] = CellStateType.WHITE;
+				}
+			}
+		}
+	}
+	
+	public void DrawField(){
+		Manager.DrawFieldS ();
+	}
+	
+	public static void DrawFieldS(){
+		for (int row = 0; row < ROW_COUNT; row++) {
+			for (int col = 0; col < COL_COUNT; col++) {
+				
+				string goName = string.Format(CELL_NAME_FORMAT, row, col);
+				GameObject go = GameObject.Find(goName);
+				
+				SpriteRenderer sr = go.GetComponent<SpriteRenderer>();
+				string imageName = "SpriteSample_" + (int)fieldState[row,col];
+				sr.sprite = GetSprite("Images/SpriteSample", imageName);
+			}
+		}
+	}
+	
+	void OnMouseDown(){
+		
+	}
+	
+	// スプライトの取得
+	// @param fileName ファイル名
+	// @param spriteName スプライト名
+	public static Sprite GetSprite(string fileName, string spriteName) {
+		Sprite[] sprites = Resources.LoadAll<Sprite>(fileName);
+		return System.Array.Find<Sprite>(sprites, (sprite) => sprite.name.Equals(spriteName));
+	}
+}
